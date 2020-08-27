af7639e9325b62e1ad90d3911cf7f98633128074
Syrym Zhauken <syrym.zhauken@gmail.com>
2020-08-27 13:20:40 -0700

    couldnt add another test

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,9 +5,8 @@ const API = `https://api.thecatapi.com/v1/breeds/search`;
 const fetchBreedDescription = (breedName, callback) => {
   
   (request(API + `/?q=${breedName}`, (error,response, body) => {
-  
     const data = JSON.parse(body);
-    callback(error, data[0].description);
+    callback(error, data[0]['description']);
   })
   );
 };
--- test/breedFetcher.test.js
+++ test/breedFetcher.test.js
@@ -15,8 +15,8 @@ describe("fetchBreedDescription", () => {
     });
   });
 
-  it('returns a string description for a valid breed, via callback', (done) => {
-    fetchBreedDescription('Siberia', (err, desc) => {
+  it('returns a string description for an invalid breed, via callback', (done) => {
+    fetchBreedDescription('blah', (err, desc) => {
       assert.equal(err, null);
 
       const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
