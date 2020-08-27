b6f995e9efc44f4a5db6f680179a78504cd2c5ff
Syrym Zhauken <syrym.zhauken@gmail.com>
2020-08-27 20:16:35 +0000

    one test added

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,15 @@
 const request = require('request');
 
-const breed = process.argv.slice(2).toString();
-
 const API = `https://api.thecatapi.com/v1/breeds/search`;
 
-request(API + `/?q=${breed}`, (error, response, body) => {
-  if (error) console.log(error);
-  console.log("response: ", response && response.statusCode);
+const fetchBreedDescription = (breedName, callback) => {
+  
+  (request(API + `/?q=${breedName}`, (error,response, body) => {
+  
+    const data = JSON.parse(body);
+    callback(error, data[0].description);
+  })
+  );
+};
 
-  const data = JSON.parse(body);
-    
-  console.log(data);
-});
+module.exports = { fetchBreedDescription };
